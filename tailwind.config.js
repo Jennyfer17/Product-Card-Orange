4d80401b2930553ac22ead680431f0e16eb21668
Jennyfer17 <tholejennyfer@gmail.com>
2024-11-28 17:06:51 +0200

    updating repository

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        my_teal: '#008080',
+        pale_orange: "#ffd9a6",
+        light_orange: "#fbb03b",
+        orange: "#f7931e",
+      },
+      fontFamily: {
+        sans: ['Josefin Sans', 'sans-serif'],
+        title: ['Playfair Display SC', 'serif'],
+      }
+    },
+  },
+  plugins: [],
+}
+
